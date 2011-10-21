2902a5575aa9bd163b0f2d798452789af7a620de
Mikko Heikkinen <hawk@biomi.org>
2011-10-21 21:59:01 +0300

    Kiinni kaikista elementeistä

--- myscript.js
+++ myscript.js
@@ -1,5 +1,13 @@
 
-document.getElementById("ctl00_ContentPlaceHolderLeftCol_toplists1_toplistFlight1_ctl00_price_link").style.background='red !important';
+var i = 0;
+while (i < 10)
+{
+	document.getElementById("ctl00_ContentPlaceHolderLeftCol_toplists1_toplistFlight1_ctl0" + i + "_price_link").style.background='red !important';
+	i++;
+}
+
+
+
 
 var greeting = "hola, ";
 var person_name = "Roberto";
