2c49a11659f0704bdf67450f4f7cdc75d99914d6
Mikko Heikkinen <hawk@biomi.org>
2011-10-21 23:14:18 +0300

    Testing JSON, not working

--- myscript.js
+++ myscript.js
@@ -8,7 +8,14 @@ while (i < 10)
 //	jQuery.getJSON( url [, data] [, success(data, textStatus, jqXHR)] );
 
 	var inner = document.getElementById("ctl00_ContentPlaceHolderLeftCol_toplists1_toplistFlight1_ctl0" + i + "_price_link").innerHTML;
-	var newinner = inner + " <b>CO<sup>2</sup><img width='20' height='5' src='http://www.flygstolen.se/images/icons/twitter_02.png' /></b>";
+
+$.getJSON("http://www.luomus.fi/projects/carbonflight/get/?",
+  {
+    from: "Stockholm",
+    to: "Phuket"
+  },
+  var newinner = inner + " <b>CO<sup>2</sup><img width='20' height='5' src='http://www.flygstolen.se/images/icons/twitter_02.png' /></b>";
+);
 	document.getElementById("ctl00_ContentPlaceHolderLeftCol_toplists1_toplistFlight1_ctl0" + i + "_price_link").innerHTML = newinner;
 	i++;
 }
