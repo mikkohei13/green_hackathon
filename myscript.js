5326e2531c78a430d45f62649362ba08de415ed6
Petri Kola <petri.kola@gmail.com>
2011-10-21 18:41:46 +0200

    Flygstolen.se otsikko reagoi klikkaukseen

--- myscript.js
+++ myscript.js
@@ -1,6 +1,8 @@
 var greeting = "hola, ";
-var button = document.getElementById("mybutton");
+var button = document.getElementById("ctl00_ContentPlaceHolderLeftCol_searchFirstPage_1_SearchHeader");
 button.person_name = "Roberto";
 button.addEventListener("click", function() {
   alert(greeting + button.person_name + ".");
 }, false);
+
+
