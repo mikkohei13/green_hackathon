73dc053f9753537f6953f7ef9320553fa1b0dad9
Mikko Heikkinen <hawk@biomi.org>
2011-10-21 22:53:55 +0300

    Modifying the page: adding content

--- myscript.js
+++ myscript.js
@@ -2,18 +2,15 @@
 var i = 0;
 while (i < 10)
 {
-	document.getElementById("ctl00_ContentPlaceHolderLeftCol_toplists1_toplistFlight1_ctl0" + i + "_price_link").style.background='red !important';
+// Changing style
+//	document.getElementById("ctl00_ContentPlaceHolderLeftCol_toplists1_toplistFlight1_ctl0" + i + "_price_link").style.background='red !important';
+
+//	jQuery.getJSON( url [, data] [, success(data, textStatus, jqXHR)] );
+
+	var inner = document.getElementById("ctl00_ContentPlaceHolderLeftCol_toplists1_toplistFlight1_ctl0" + i + "_price_link").innerHTML;
+	var newinner = inner + " <b>CO<sup>2</sup><img width='20' height='5' src='http://www.flygstolen.se/images/icons/twitter_02.png' /></b>";
+	document.getElementById("ctl00_ContentPlaceHolderLeftCol_toplists1_toplistFlight1_ctl0" + i + "_price_link").innerHTML = newinner;
 	i++;
 }
-
-
-
-
-var greeting = "hola, ";
-var person_name = "Roberto";
-var button = document.getElementById("ctl00_ContentPlaceHolderLeftCol_toplists1_toplistFlight1_ctl00_price_link");
-button.addEventListener("click", function() {
-  alert(greeting + person_name + ".");
-}, false);
 
 
