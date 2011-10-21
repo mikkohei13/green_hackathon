bdd7af2ac068ddc84aab4834746d343f4da9a6fe
Mikko Heikkinen <hawk@biomi.org>
2011-10-21 23:45:32 +0300

    Testing JSON

--- myscript.js
+++ myscript.js
@@ -14,8 +14,13 @@ $.getJSON("http://www.luomus.fi/projects/carbonflight/get/?",
     from: "Stockholm",
     to: "Phuket"
   },
-  var newinner = inner + " <b>CO<sup>2</sup><img width='20' height='5' src='http://www.flygstolen.se/images/icons/twitter_02.png' /></b>";
+  function (data)
+  {
+  alert(data);
+//  var newinner = inner + " <b>CO<sup>2</sup><img width='20' height='5' src='http://www.flygstolen.se/images/icons/twitter_02.png' /></b>";
+  }
 );
+  
 	document.getElementById("ctl00_ContentPlaceHolderLeftCol_toplists1_toplistFlight1_ctl0" + i + "_price_link").innerHTML = newinner;
 	i++;
 }
